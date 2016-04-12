64165bcaa04e03f1744adbf0183a3e7cc09c2ea9
Jędrzej Nowak <mail@jedrzejnowak.pl>
2016-04-13 00:22:30 +0200

    Containers instead of packages

--- setup_k8s.py
+++ setup_k8s.py
@@ -11,35 +11,45 @@ from itertools import combinations
 
 
 def setup_master():
-    config = cr.create('kube-config', 'k8s/global_config', {'dns_cluster_ip': '10.254.0.10',
-                                                            'dns_domain': 'cluster.local'})[0]
+    config = cr.create('kube-config', 'k8s/global_config', {'cluster_dns': '10.254.0.10',
+                                                            'cluster_domain': 'cluster.local'})[0]
     master = cr.create('kube-node-master', 'k8s/node', {'name': 'kube-node-master',
-                                                         'ip': '10.0.0.3',
-                                                         'ssh_user': 'vagrant',
-                                                         'ssh_password': 'vagrant',
-                                                         'ssh_key': None})['kube-node-master']
-    etcd = cr.create('etcd', 'k8s/etcd', {'listen_client_port': 4001})['etcd']
-    master.connect(etcd, {'name': 'listen_client_host'})
-    etcd.connect(etcd, {'listen_client_host': 'listen_client_url',
-                        'listen_client_port': 'listen_client_url'})
+                                                        'ip': '10.0.0.3',
+                                                        'ssh_user': 'vagrant',
+                                                        'ssh_password': 'vagrant',
+                                                        'ssh_key': None})['kube-node-master']
+    # etcd = cr.create('etcd', 'k8s/etcd', {'listen_client_port': 4001})['etcd']
+    # master.connect(etcd, {'name': 'listen_client_host'})
+    # etcd.connect(etcd, {'listen_client_host': 'listen_client_url',
+    #                     'listen_client_port': 'listen_client_url'})
+    #                     # 'listen_client_port_events': 'listen_client_url_events',
+    #                     # 'listen_client_host': 'listen_client_url_events'})
 
-    kubernetes = cr.create('k8s-master', 'k8s/kubernetes', {'master_port': 8080})['k8s-master']
-    master.connect(kubernetes, {'name': 'master_host'})
-    etcd.connect(kubernetes, {'listen_client_url': 'etcd_servers'})
-    kubernetes.connect(kubernetes, {'master_port': 'master_address',
-                                    'master_host': 'master_address'})
-
-    calico = cr.create('calico-master', 'k8s/calico', {})['calico-master']
-    master.connect(calico, {'ip': 'ip'})
-    etcd.connect(calico, {'listen_client_url': 'etcd_authority'})
-    calico.connect(calico, {'etcd_authority': 'etcd_authority_internal'})
     master.connect(config, {})
+    docker = cr.create('kube-docker-master', 'k8s/docker')['kube-docker-master']
+    master.connect(docker, {})
+
+    kubelet = cr.create('kubelet-master', 'k8s/kubelet_master')['kubelet-master']
+
+    calico = cr.create('calico-master', 'k8s/calico_master', {'options': "--nat-outgoing --ipip"})['calico-master']
+    master.connect(calico, {'ip': ['ip', 'etcd_host']})
+    config.connect(calico, {'network': 'network',
+                            'prefix': 'prefix'})
+    calico.connect(calico, {'etcd_host': 'etcd_authority',
+                            'etcd_port': 'etcd_authority',
+                            'etcd_authority': 'etcd_authority_internal'})
+    config.connect(kubelet, {'service_cluster_ip_range': "service_cluster_ip_range"})
+    master.connect(kubelet, {'name': 'master_host'})
+    kubelet.connect(kubelet, {'master_host': 'master_address',
+                              'master_port': 'master_address'})
+
+    add_event(Dep(docker.name, 'run', 'success', kubelet.name, 'run'))
+    add_event(Dep(kubelet.name, 'run', 'success', calico.name, 'run'))
 
 
 def setup_nodes(num=1):
     kube_nodes = []
-    etcd = rs.load('etcd')
-    kubernetes_master = rs.load('k8s-master')
+    kubernetes_master = rs.load('kubelet-master')
     calico_master = rs.load('calico-master')
     config = rs.load('kube-config')
     for i in xrange(num):
@@ -58,32 +68,26 @@ def setup_nodes(num=1):
             'kube-node-%d-iface' % j,
             'k8s/virt_iface',
             {'name': 'cbr0',
-             'netmask': '255.255.255.0',
-             'ipaddr': '192.168.%d.1' % (i + 1),
+             'ipaddr': '192.168.%d.1' % (i + 1),  # TODO(jnowak) support config for it
              'onboot': 'yes',
              'bootproto': 'static',
              'type': 'Bridge'})['kube-node-%d-iface' % j]
         kube_node.connect(iface_node, {})
 
+        config.connect(iface_node, {'netmask': 'netmask'})
+
         calico_node = cr.create('calico-node-%d' % j, 'k8s/calico', {})[0]
 
-        add_event(Dep(calico_master.name, 'run', 'success', calico_node.name, 'run'))
-
         kube_node.connect(calico_node, {'ip': 'ip'})
-
+        calico_master.connect(calico_node, {'etcd_authority': 'etcd_authority'})
         calico_node.connect(calico_node, {
             'etcd_authority': 'etcd_authority_internal'
         })
-
-        etcd.connect(calico_node, {'listen_client_url': 'etcd_authority'})
-
         calico_cni = cr.create('calico-cni-node-%d' % j, 'k8s/cni', {})[0]
         calico_node.connect(calico_cni, {'etcd_authority_internal': 'etcd_authority'})
 
         docker = cr.create('kube-docker-%d' % j,
                            'k8s/docker')['kube-docker-%d' % j]
-
-        add_event(Dep(docker.name, 'run', 'success', calico_node.name, 'run'))
 
         kube_node.connect(docker, {})
         iface_node.connect(docker, {'name': 'iface'})
@@ -94,9 +98,12 @@ def setup_nodes(num=1):
 
         kube_node.connect(kubelet, {'name': 'kubelet_hostname'})
         kubernetes_master.connect(kubelet, {'master_address': 'master_api'})
-        calico_node.connect(kubelet, {'etcd_authority_internal': 'etcd_authority'})
-        config.connect(kubelet, {'dns_domain': 'dns_domain',
-                                 'dns_cluster_ip': 'dns_cluster_ip'})
+        config.connect(kubelet, {'cluster_domain': 'cluster_domain',
+                                 'cluster_dns': 'cluster_dns'})
+
+        add_event(Dep(docker.name, 'run', 'success', calico_node.name, 'run'))
+        add_event(Dep(docker.name, 'run', 'success', kubelet.name, 'run'))
+        add_event(Dep(calico_node.name, 'run', 'success', kubelet.name, 'run'))
 
         kube_nodes.append(kube_node)
     kube_master = rs.load('kube-node-master')
@@ -112,7 +119,7 @@ def setup_nodes(num=1):
 
 def add_dashboard():
     kube_master = rs.load('kube-node-master')
-    master = rs.load('k8s-master')
+    master = rs.load('kubelet-master')
     dashboard = cr.create('kubernetes-dashboard', 'k8s/dashboard', {})[0]
     master.connect(dashboard, {'master_port': 'api_port'})
     kube_master.connect(dashboard, {'ip': 'api_host'})
@@ -121,11 +128,12 @@ def add_dashboard():
 def add_dns():
     config = rs.load('kube-config')
     kube_master = rs.load('kube-node-master')
-    master = rs.load('k8s-master')
+    master = rs.load('kubelet-master')
     kube_dns = cr.create('kube-dns', 'k8s/kubedns', {})[0]
     master.connect(kube_dns, {'master_port': 'api_port'})
     kube_master.connect(kube_dns, {'ip': 'api_host'})
-    config.connect(kube_dns, {'dns_domain': 'dns_domain'})
+    config.connect(kube_dns, {'cluster_domain': 'cluster_domain',
+                              'cluster_dns': 'cluster_dns'})
 
 
 setup_master()
