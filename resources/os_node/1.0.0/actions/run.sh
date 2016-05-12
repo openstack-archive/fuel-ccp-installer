9cf08fb6075c28f5df957b9472b7e62f3da9f478
Jedrzej Nowak <jnowak@mirantis.com>
2016-05-12 14:40:31 +0200

    Set hostname on fedora
    Change-Id: I67de962bcd96883daf14557923f322a9c18521b5

--- resources/os_node/1.0.0/actions/run.sh
+++ resources/os_node/1.0.0/actions/run.sh
@@ -4,6 +4,7 @@ if [[ ! "$os" == "Ubuntu" ]]; then
     sudo dnf install -y python python-dnf ansible libselinux-python
     sudo /usr/sbin/setenforce 0 || echo 'ok'
     sudo systemctl stop firewalld.service || echo 'ok'
+    sudo hostnamectl set-hostname --static "{{name}}"
 else
     if [[ "$os" == "Ubuntu" ]]; then
         sudo apt-get install -y python ansible
