c470f18a64ea341429873028ddfe489f94d8e89d
Marek Zawadzki <mzawadzki@mirantis.com>
2016-06-08 16:49:16 +0200

    Set number of slaves from env if possible.
    Change-Id: I5bbf2ca005904b96aace44af91d4b2b20fafe838

--- utils/jenkins/run_k8s_deploy_test.sh
+++ utils/jenkins/run_k8s_deploy_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-export SLAVES_COUNT=3
+export SLAVES_COUNT=${SLAVES_COUNT:-3}
 export DEPLOY_TIMEOUT=1200
 export TEST_SCRIPT="/usr/bin/python mcpinstall.py deploy --dns --dashboard"
 
