15d0c5000d0495d495768b62f345cd75ae0f83f0
Proskurin Kirill <kproskurin@mirantis.com>
2016-06-14 15:43:39 +0000

    (Solar) Replace ubuntu docker with official docker
    Change-Id: I70055b7ee9bfe7b810f37345c3cdc00de2ab3873

--- utils/packer/ubuntu16.04/scripts/setup.sh
+++ utils/packer/ubuntu16.04/scripts/setup.sh
@@ -6,3 +6,8 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
 # Set vim as a default editor
 update-alternatives --set editor /usr/bin/vim.basic
+
+# Add official docker repos
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
