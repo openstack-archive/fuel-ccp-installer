00304da4b30763b7fafe27de7c4951d93ca00d7a
Proskurin Kirill <kproskurin@mirantis.com>
2016-06-14 14:32:57 +0000

    Add vim as a default editor
    Change-Id: Ia1a0abd9d7e7663899203078f7d8a28676a29313

--- utils/packer/ubuntu16.04/scripts/setup.sh
+++ utils/packer/ubuntu16.04/scripts/setup.sh
@@ -3,3 +3,6 @@
 # Set up sudo
 echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+# Set vim as a default editor
+update-alternatives --set editor /usr/bin/vim.basic
