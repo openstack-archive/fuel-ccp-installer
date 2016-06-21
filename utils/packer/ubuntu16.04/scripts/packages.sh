7c7a94fe7e307ded5b9c4a7f07853c4686dc22c7
Ubuntu <mzawadzki@mirantis.com>
2016-06-21 10:49:05 +0000

    Improvements to scripts building base VM image with packer. Please note the assumption that Docker is installed later on with Kargo.
    Change-Id: I10068d3f12cd78a90ea49f69a90bcb292aa22a25

--- utils/packer/ubuntu16.04/scripts/packages.sh
+++ utils/packer/ubuntu16.04/scripts/packages.sh
@@ -1,3 +1,10 @@
+# add Docker repo:
+#apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#cat > /etc/apt/sources.list.d/docker.list <<EOF
+#deb https://apt.dockerproject.org/repo ubuntu-trusty main
+#deb https://apt.dockerproject.org/repo ubuntu-xenial main
+#EOF
+
 apt-get -y update
 apt-get -y dist-upgrade
 
@@ -9,8 +16,9 @@ nfs-common
 vim
 python-pip
 git-review
-tox
+python-tox
 screen
 tmux
 "
+#PACKAGES="${PACKAGES} docker-engine"
 apt-get -y install $PACKAGES
