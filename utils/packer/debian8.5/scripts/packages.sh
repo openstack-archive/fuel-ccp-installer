eba5a155ccada9b2017e92b962ab74a975c264bb
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-07-20 12:47:42 +0200

    Fix packer scripts
    Sync scripts accross debian/ubuntu cases.
    Pre-install ansible, kargo-cli, MCP/CCP CLI build time
    to speed up dev/CI.
    
    Change-Id: I74483725d13fbb1444f9f9bbc5a0c4b817807225
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- utils/packer/debian8.5/scripts/packages.sh
+++ utils/packer/debian8.5/scripts/packages.sh
@@ -1,4 +1,28 @@
 #!/bin/bash -eux
+#FIXME(bogdando) switch to jessie-backports
+cat > /etc/apt/preferences.d/testing << EOF
+Package: ansible
+Pin: release a=testing
+Pin-Priority: 1001
+
+Package: python-setuptools
+Pin: release a=testing
+Pin-Priority: 1001
+
+Package: python-pkg-resources
+Pin: release a=testing
+Pin-Priority: 1001
+
+Package: *
+Pin: release a=testing
+Pin-Priority: 100
+EOF
+
+cat > /etc/apt/sources.list.d/testing.list << EOF
+deb http://http.us.debian.org/debian testing main
+deb-src http://http.us.debian.org/debian testing main
+EOF
+
 apt-get -y update
 apt-get -y dist-upgrade
 
@@ -13,6 +37,17 @@ git-review
 python-tox
 screen
 tmux
+python-dev
+gcc
+libssl-dev
+libffi-dev
+software-properties-common
+ansible
+python-setuptools
 "
 #PACKAGES="${PACKAGES} docker-engine"
 apt-get -y install $PACKAGES
+
+#Installer/CCP tools
+pip install git+https://github.com/kubespray/kargo-cli.git --upgrade
+pip install git+https://git.openstack.org/openstack/fuel-ccp.git --upgrade
