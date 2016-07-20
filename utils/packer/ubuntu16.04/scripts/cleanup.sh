806e682d8ee1f19294e84358fe68aaedb59aa6b4
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-07-20 12:47:42 +0200

    Fix packer scripts
    * Sync scripts accross debian/ubuntu cases.
    * Pre-install ansible, python setuptools, dependencies for
    kargo-cli, MCP/CCP CLI build time to speed up dev/CI.
    * Add ethtool requirement for a kubelet
    
    Change-Id: I74483725d13fbb1444f9f9bbc5a0c4b817807225
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- utils/packer/ubuntu16.04/scripts/cleanup.sh
+++ utils/packer/ubuntu16.04/scripts/cleanup.sh
@@ -9,6 +9,29 @@ sed '/ens/d' -i /etc/network/interfaces
 echo 'auto eth0' >> /etc/network/interfaces
 echo 'iface eth0 inet dhcp' >> /etc/network/interfaces
 
-# Apt cleanup.
-apt autoremove
-apt update
+apt-get -y autoremove --purge
+find /var/cache -type f -exec rm -rf {} \;
+find /var/lib/apt -type f | xargs rm -f
+
+rm -rf /dev/.udev/
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+mkdir -p /etc/udev/rules.d/70-persistent-net.rules
+
+if [ -d "/var/lib/dhcp" ]; then
+    rm -f /var/lib/dhcp/*
+fi
+
+rm -rf /tmp/*
+
+unset HISTFILE
+rm -f /root/.bash_history
+rm -f /home/vagrant/.bash_history
+
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+
+>/var/log/lastlog
+>/var/log/wtmp
+>/var/log/btmp
+
+sync
