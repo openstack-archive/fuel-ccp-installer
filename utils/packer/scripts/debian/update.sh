e986290189ba2b851cc96e57c8904d9f5456585d
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-08-01 18:24:13 +0200

    Use httpredir for debian 8
    Use only http://httpredir.debian.org/ over cdn.debian.net
    or fixed mirrors
    
    Change-Id: Ieb307de6ad971e385151e49e23544f56127b17ca
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- utils/packer/scripts/debian/update.sh
+++ utils/packer/scripts/debian/update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -eux
 
+# Use only http://httpredir.debian.org/ over cdn.debian.net or fixed mirrors
+cat > /etc/apt/sources.list << EOF
+deb http://httpredir.debian.org/debian jessie main
+deb http://httpredir.debian.org/debian jessie-updates main
+deb http://httpredir.debian.org/debian jessie-backports main
+deb http://security.debian.org jessie/updates main
+EOF
+
 if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
   	echo "==> Updating list of repositories"
     # apt-get update does not actually perform updates, it just downloads and indexes the list of packages
