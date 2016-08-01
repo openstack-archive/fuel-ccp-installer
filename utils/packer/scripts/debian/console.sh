8b7f029fb2179085ae9d5eed7b0e67de3a741710
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-08-01 18:50:56 +0300

    Fix grub config writer for debian
    Change-Id: I8effd136fe65f8cdb1faab8408f51064fe590ece

--- utils/packer/scripts/debian/console.sh
+++ utils/packer/scripts/debian/console.sh
@@ -7,4 +7,4 @@ GRUB_CMDLINE_LINUX='0 console=ttyS0,19200n8 cgroup_enable=memory swapaccount=1'
 GRUB_SERIAL_COMMAND="serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1"
 EOF
 
-grub-mkconfig -o /etc/default/grub
+update-grub2
