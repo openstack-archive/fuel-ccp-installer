70e94e4f7e758daeb9d084365cded9ccbcd3a6f0
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-08-11 18:13:16 +0300

    Fix ansible and apt cache in packer image
    Empty cache leads to slower CI deployments.
    Ansible version was 2.0.0, but Kargo requires 2.1.0.
    
    Change-Id: Ide54126f30226b9c9a99ccbf568f9b47a02c4562

--- utils/packer/scripts/debian/packages.sh
+++ utils/packer/scripts/debian/packages.sh
@@ -15,11 +15,11 @@ screen
 sshpass
 tmux
 python-dev
+python-netaddr
 gcc
 libssl-dev
 libffi-dev
 software-properties-common
-ansible
 python-setuptools
 "
 
@@ -31,3 +31,9 @@ curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 python /tmp/get-pip.py
 rm /tmp/get-pip.py
 pip install --upgrade pip
+
+# Install ansible
+# Retry because launchpad errors sometimes
+until apt-add-repository -y ppa:ansible/ansible; do sleep 1; done
+apt-get -y install ansible
+
