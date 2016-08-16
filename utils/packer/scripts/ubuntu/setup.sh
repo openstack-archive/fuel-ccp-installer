2638098f02209e5e77cf65a2c3037c9fffb27ca4
Marek Zawadzki <mzawadzki@mirantis.com>
2016-08-16 16:21:19 +0200

    Include docker-engine and hyperkube in VM image.
    This is optimization to reduce deployment time of development environments.
    Since Docker and Hyperkube are installed every time by Kargo during deployment
    of K8s we can include them into the image during its build time.
    This patch deduces correct versions by parsing Kargo yaml files from github.
    This patch covers Ubuntu only and should not break Debian compatibility.
    
    Change-Id: I40e4e89e7ffd28eb344a50ee7927778456cd6cc9

---
+++ utils/packer/scripts/ubuntu/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eux
+
+echo "==> Setting up sudo"
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+echo "==> Configuring logging"
+touch /var/log/daemon.log
+chmod 666 /var/log/daemon.log
+echo "daemon.* /var/log/daemon.log" >> /etc/rsyslog.d/50-default.conf
+
+echo "==> Setting vim as a default editor"
+update-alternatives --set editor /usr/bin/vim.basic
