6f9572e9ba6381a7414d3241b31d5ce52e1fce3a
Marek Zawadzki <mzawadzki@mirantis.com>
2016-08-16 16:21:19 +0200

    Include docker-engine and hyperkube in VM image.
    This is optimization to reduce deployment time of development environments.
    Since Docker and Hyperkube are installed every time by Kargo during deployment
    of K8s we can include them into the image during its build time.
    This patch deduces correct versions by parsing Kargo yaml files from github.
    This patch covers Ubuntu only and should not break Debian compatibility.
    
    Change-Id: I40e4e89e7ffd28eb344a50ee7927778456cd6cc9

---
+++ utils/packer/scripts/debian/ubuntu-16.04/packages.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -eux
+
+apt-get update
+apt-get -y dist-upgrade
+
+PACKAGES="
+curl
+ethtool
+htop
+isc-dhcp-client
+nfs-common
+vim
+git-review
+python-tox
+screen
+sshpass
+tmux
+python-dev
+python-netaddr
+software-properties-common
+python-setuptools
+"
+
+echo "==> Installing packages"
+apt-get -y install $PACKAGES
+
+# Install pip
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python /tmp/get-pip.py
+rm /tmp/get-pip.py
+pip install --upgrade pip
+
+# Preinstall Docker version required by Kargo:
+KARGO_DEFAULT_UBUNTU_URL="https://raw.githubusercontent.com/openstack/fuel-ccp-installer/master/utils/kargo/kargo_default_ubuntu.yaml"
+DOCKER_REQUIRED_VERSION=`wget --output-document=- ${KARGO_DEFAULT_UBUNTU_URL}\
+| awk '/docker_version/ {gsub(/"/, "", $NF); print $NF}'`
+apt-get -y install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \
+--recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" \
+> /etc/apt/sources.list.d/docker.list
+apt-get update
+DOCKER_APT_PACKAGE_VERSION=`apt-cache policy docker-engine \
+| grep ${DOCKER_REQUIRED_VERSION} | head -1 \
+| grep -o "${DOCKER_REQUIRED_VERSION}.*\ "`
+DOCKER_APT_PACKAGE_FULL_NAME="docker-engine"
+# FIXME(mzawadzki): this is workaround for non-maching Docker version
+[ ! -z ${DOCKER_APT_PACKAGE_VERSION} ] \
+&& DOCKER_APT_PACKAGE_FULL_NAME+="="${DOCKER_APT_PACKAGE_VERSION}
+apt-get -y install ${DOCKER_APT_PACKAGE_FULL_NAME}
+
+# Pull K8s hyperkube image version required by Kargo:
+KARGO_DEFAULT_COMMON_URL="https://raw.githubusercontent.com/openstack/fuel-ccp-installer/master/utils/kargo/kargo_default_common.yaml"
+TMP_FILE=output_${RANDOM}.yaml
+wget --output-document ${TMP_FILE} ${KARGO_DEFAULT_COMMON_URL}
+HYPERKUBE_IMAGE_NAME=`awk '/hyperkube_image_repo/ {gsub(/"/, "", $NF); \
+print $NF}' ${TMP_FILE}`
+HYPERKUBE_IMAGE_TAG=`awk '/hyperkube_image_tag/ {gsub(/"/, "", $NF); \
+print $NF}' ${TMP_FILE}`
+rm ${TMP_FILE}
+docker pull ${HYPERKUBE_IMAGE_NAME}:${HYPERKUBE_IMAGE_TAG}
