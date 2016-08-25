4eeb80b21355dd9fd19bacc7359c7d79107a16c8
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-08-25 15:48:55 +0200

    Make pull images optional and pull more
    Pull the most sized images into the VM boxes
    packer build time. Make this optional, defaults True.
    
    Change-Id: Iee1c707c8bc9f17bc3cfffb9d167908e47b7ccb3
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- utils/packer/scripts/debian/ubuntu-16.04/packages.sh
+++ utils/packer/scripts/debian/ubuntu-16.04/packages.sh
@@ -49,6 +49,7 @@ DOCKER_APT_PACKAGE_FULL_NAME="docker-engine"
 && DOCKER_APT_PACKAGE_FULL_NAME+="="${DOCKER_APT_PACKAGE_VERSION}
 apt-get -y install ${DOCKER_APT_PACKAGE_FULL_NAME}
 
+[ "${PULL_IMAGES}" = "true" ] || exit 0
 # Pull K8s hyperkube image version required by Kargo:
 KARGO_DEFAULT_COMMON_URL="https://raw.githubusercontent.com/openstack/fuel-ccp-installer/master/utils/kargo/kargo_default_common.yaml"
 TMP_FILE=output_${RANDOM}.yaml
@@ -57,5 +58,19 @@ HYPERKUBE_IMAGE_NAME=`awk '/hyperkube_image_repo/ {gsub(/"/, "", $NF); \
 print $NF}' ${TMP_FILE}`
 HYPERKUBE_IMAGE_TAG=`awk '/hyperkube_image_tag/ {gsub(/"/, "", $NF); \
 print $NF}' ${TMP_FILE}`
-rm ${TMP_FILE}
 docker pull ${HYPERKUBE_IMAGE_NAME}:${HYPERKUBE_IMAGE_TAG}
+
+# Pull K8s calico images version required by Kargo:
+CALICO_IMAGE_TAG=`awk '/calico_version/ {gsub(/"/, "", $NF); \
+print $NF}' ${TMP_FILE}`
+docker pull calico/ctl:${CALICO_IMAGE_TAG}
+docker pull calico/node:${CALICO_IMAGE_TAG}
+
+# Pull K8s etcd image version required by Kargo:
+ETCD_IMAGE_TAG=`awk '/etcd_version/ {gsub(/"/, "", $NF); \
+print $NF}' ${TMP_FILE}`
+docker pull quay.io/coreos/etcd:${ETCD_IMAGE_TAG}
+
+# Pull required images w/o version preferences:
+docker pull gcr.io/google_containers/kubernetes-dashboard-amd64:v1.1.0
+rm ${TMP_FILE}
