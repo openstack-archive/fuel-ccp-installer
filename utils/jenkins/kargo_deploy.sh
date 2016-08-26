08a2b96df509f347aac1d452d8e57b16faff13da
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-08-26 18:57:27 +0400

    Remove writing id_rsa to ~/.ssh
    This is no longer necessary because there is an ssh-add used when
    using an autocreated ssh key.
    
    Change-Id: I249a16c1be1c8cb5ac50afab65bc85978e8c627f

--- utils/jenkins/kargo_deploy.sh
+++ utils/jenkins/kargo_deploy.sh
@@ -194,12 +194,6 @@ echo "Checking out kargo playbook..."
 admin_node_command "sh -c 'git clone $KARGO_REPO $ADMIN_WORKSPACE/kargo'" || true
 admin_node_command "sh -c 'cd $ADMIN_WORKSPACE/kargo && git fetch --all && git checkout $KARGO_COMMIT'"
 
-if [ -z "$INHERIT_SSH_AGENT" ]; then
-   cat $WORKSPACE/id_rsa | admin_node_command "cat - > .ssh/id_rsa"
-   admin_node_command chmod 600 .ssh/id_rsa
-fi
-
-
 # Try to get IPs from inventory if it isn't provided
 # Use git repo if available, then residual files from previous deploy, then
 # finally generate new inventory.
