a37706e64041e144c3b70601eccdea6e5b44737f
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-09-06 15:41:33 +0200

    Reduce verbosity of collect logs task
    Change-Id: If090cbb306fe3eb6d5388d35fccca1ceb423c0bd
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- utils/jenkins/kargo_deploy.sh
+++ utils/jenkins/kargo_deploy.sh
@@ -39,7 +39,7 @@ required_ansible_version="2.1.0"
 
 function collect_info {
     # Get diagnostic info and store it as the logs.tar.gz at the admin node
-    admin_node_command ADMIN_USER=$ADMIN_USER \
+    admin_node_command ADMIN_USER=$ADMIN_USER LOG_LEVEL="" \
         ADMIN_WORKSPACE=$ADMIN_WORKSPACE collect_logs.sh
 }
 
