e6473b0534b06e04258c48945773c28e7f4519a2
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-09-06 13:08:27 +0300

    Set default image to ubuntu and fix default path
    Change-Id: I3c4c86f2d6772343825f985657b7c7b3ab16a15d

--- utils/packer/scripts/minimize.sh
+++ utils/packer/scripts/minimize.sh
@@ -5,5 +5,5 @@ nice -n 19 ionice -c2 -n7 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
 # Make sure we wait until all the data is written to disk, otherwise
-# Packer might quite too early before the large files are deleted
+# Packer might quit too early before the large files are deleted
 sync
