9583f7378ab1bbccb907e372b1b9e700a2660ca0
Oleg Basov <obasov@mirantis.com>
2016-09-16 02:56:15 +0300

    Fixing wrong ansible forks number calculation. Also, setting it up to 150 in the top - working much faster than at 50. 'Max open files' limit in base OS may occur after exeeding 200 ansible forks.
    Change-Id: I9b2aa923c7f68d736988e34ea3d35593658e4eb2

--- utils/jenkins/kargo_deploy.sh
+++ utils/jenkins/kargo_deploy.sh
@@ -279,10 +279,10 @@ if ! admin_node_command git -C $ADMIN_WORKSPACE/inventory diff --cached --name-o
 fi
 
 # Calculate parallel ansible execution
-if [[ "${#SLAVE_IPS[@]}" < 50 ]]; then
+if [[ "${#SLAVE_IPS[@]}" -lt 150 ]]; then
     ANSIBLE_FORKS="${#SLAVE_IPS[@]}"
 else
-    ANSIBLE_FORKS=50
+    ANSIBLE_FORKS=150
 fi
 
 # Stop trapping pre-setup tasks
