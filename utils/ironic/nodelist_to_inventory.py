2807e3b89677cc4cc1039d764afb1f0380cd7c87
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-12-30 14:42:17 +0300

    Add support for ironic nodelist as inventory source
    Change-Id: I5d2dcd58409429fd8a71c9f5713a67fc032bdcd2

---
+++ utils/ironic/nodelist_to_inventory.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Converts Ironic YAML output to Ansible YAML inventory
+# Example input:
+#   openstack --os-cloud=bifrost baremetal node list -f yaml --noindent \
+#   --fields name instance_info | python nodelist_to_inventory.py
+
+import sys
+import yaml
+
+nodes = yaml.load(sys.stdin.read())
+
+groups = {}
+for node in nodes:
+    for tag in node['Instance Info']['tags']:
+        if tag not in groups.keys():
+            groups[tag] = {}
+        ip = node['Instance Info']['ipv4_address']
+        groups[tag][node['Name']] = {'ip': ip}
+print yaml.dump(groups, indent=2, default_flow_style=False)
