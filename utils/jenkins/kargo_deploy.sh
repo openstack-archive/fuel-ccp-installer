1f4f4804fa0ae5c9c63b488c5955b83825faa159
Artur Zarzycki <azarzycki@mirantis.com>
2017-01-17 21:47:59 +0100

    Install ansible 2.2.0 from pip
    After removing ansible 2.2.0 for xenial from
    http://ppa.launchpad.net/ansible/ansible/ubuntu/pool/main/a/ansible/
    we need to find another workaround until
    https://github.com/kubernetes-incubator/kargo/pull/910
    will be merge. So proposition is to install ansible with pip.
    
    Change-Id: Ie392ebf84f1b36335e0d029b66f4f2c1960aba8d

--- utils/jenkins/kargo_deploy.sh
+++ utils/jenkins/kargo_deploy.sh
@@ -234,9 +234,9 @@ if ! admin_node_command type ansible > /dev/null || \
             wait_for_apt_lock_release
             with_retries admin_node_command -- sudo apt-get update
             # (FIXME) temporary workaround until https://github.com/kubernetes-incubator/kargo/pull/910 will be merged
-            with_retries admin_node_command -- wget http://ppa.launchpad.net/ansible/ansible/ubuntu/pool/main/a/ansible/ansible_2.2.0.0-1ppa~xenial_all.deb
-            wait_for_apt_lock_release
-            with_retries admin_node_command -- sudo apt -y install ./ansible_2.2.0.0-1ppa~xenial_all.deb
+            with_retries admin_node_command -- sudo pip install ansible==2.2.0
+            with_retries admin_node_command -- sudo ln -s /usr/local/bin/ansible-playbook /usr/bin/ansible-playbook
+            with_retries admin_node_command -- sudo ln -s /usr/local/bin/ansible /usr/bin/ansible
         ;;
         debian)
             cat ${BASH_SOURCE%/*}/files/debian_backports_repo.list | admin_node_command "sudo sh -c 'cat - > /etc/apt/sources.list.d/backports.list'"
